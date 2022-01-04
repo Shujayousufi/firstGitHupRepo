d941971998ee73409e44199404202bd23695b72b
Shujayousufi <shujayusfi@gmail.com>
2022-01-04 15:08:02 -0800

    Fibnum

--- GitPractice/src/TestPackage/GitTest.java
+++ GitPractice/src/TestPackage/GitTest.java
@@ -1,5 +1,17 @@
 package TestPackage;
 
 public class GitTest {
-
+	public static void main(String[] args) {
+		
+		
+		int range = 10;
+		int [] fibnum = new int [range];
+		fibnum [0] =0;
+		fibnum [1]= 1;
+		System.out.println(fibnum[0]);
+		for (int i = 2; i < fibnum.length; i++) {
+			fibnum[i] = fibnum[i-1] + fibnum[i-2];
+			System.out.println(fibnum[i]);
+		}
+	}
 }
